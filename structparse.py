aa429ee89804d6924eb210eb96616ff4eb9ed8e5
Kamila Součková <kamila@ksp.sk>
2015-02-11 21:22:19 +0100

    first attempt at request handling core

--- structparse.py
+++ structparse.py
@@ -3,54 +3,95 @@ from struct import Struct
 from enum import Enum
 
 PROTOCOL_VERSION = bytes([0x00,0x01])
+ENDIANITY        = '<'  # little
 
-class BadRequestError(Exception): pass
+class BadMessageError(Exception): pass
+
+def checkmsg(expression, err):
+    if not expression: raise BadMessageError(err)
+
+# pieces of struct format strings -- docs.python.org/3/library/struct.html
+uint8_t = 'B'
+bytes_t = lambda sz: '{}s'.format(sz)
+
+def mystruct(name, fields, types):
+    """Creates a namedtuple that can be packed to and unpacked from `bytes`.
+
+    It simply attaches a `Struct` with the given format string.
+    """
+    cls = namedtuple(name, fields)
+    cls._struct = Struct(ENDIANITY + ''.join(types))
+
+    @classmethod
+    def unpack_from(cls, buf):
+        """Constructs a struct by unpacking the buffer.
+
+        Returns the new instance and the rest of the buffer.
+        """
+        sz = cls._struct.size
+        head, tail = buf[:sz], buf[sz:]
+        return cls(*cls._struct.unpack(head)), tail
+
+    def pack(self):
+        """Returns itself packed as `bytes`."""
+        return self._struct.pack(*self)
+
+    cls.unpack_from = unpack_from
+    cls.pack = pack
+    return cls
 
 class MsgType(Enum):
     OPEN = 1
 
-PacketHead = namedtuple('PacketHead', [
-                        'protocol_version',
-                        'controllerID',
-                        'nonce',
-])
-PacketHead._struct = Struct('<2s6s18s')
+class ReplyStatus(Enum):
+    OK        = 0x01
+    ERR       = 0x10
+    TRY_AGAIN = 0x11
+S = ReplyStatus
 
-RequestHead = namedtuple('RequestHead', [ 'msg_type' ])
-RequestHead._struct = Struct('<B')
+PacketHead = mystruct('PacketHead',
+                     ['protocol_version', 'controllerID', 'nonce'      ],
+                     [ bytes_t(2)       ,  bytes_t(6)   ,  bytes_t(18) ])
 
-ReplyHead = namedtuple('ReplyHead', [ 'msg_type', 'status' ])
-ReplyHead._struct = Struct('<BB')
+RequestHead = mystruct('RequestHead',
+                      ['msg_type'],
+                      [ uint8_t  ])
 
-def parse_into(cls, buf):
-    """TODO doc"""
-    assert hasattr(cls, '_struct') and isinstance(cls._struct, Struct)
-    size = cls._struct.size
-    head, tail = buf[:size], buf[size:]
-    s = cls._struct.unpack(head)
-    return cls(*s), tail
+ReplyHead = mystruct('ReplyHead',
+                    ['msg_type', 'status' ],
+                    [ uint8_t  ,  uint8_t ])
 
 def fstring(buf):
     length, string = int(buf[0]), buf[1:]
-    if not length <= len(string): raise BadRequestError()
+    checkmsg(length <= len(string), 'fstring length > buffer size')
     return string[:length]
 
-def readpacket(buf):
-    """Example usage."""
-    p, payload = parse_into(PacketHead, buf)
-    if not p.protocol_version == PROTOCOL_VERSION: raise BadRequestError
-    print(p)
-    # assume p is a request (usually this is determined by whether you are
-    # server or client
-    r, data = parse_into(RequestHead, payload)
-    print(r)
+process_request = {
+    MsgType.OPEN:
+        lambda data: ((S.OK if fstring(data) == 'Hello' else S.ERR), None),
+}
 
-    if MsgType(r.msg_type) == MsgType.OPEN:
-        isic_id = fstring(data)
-        print(isic_id)
-    else:
-        raise BadRequestError
+def make_reply(packet_head, request_head, status, data):
+    nnonce = bytearray(packet_head.nonce); nnonce[-1] |= 0x1
+    p = PacketHead(protocol_version=PROTOCOL_VERSION,
+                   controllerID=packet_head.controllerID,
+                   nonce=nnonce)
+    r = ReplyHead(msg_type=request_head.msg_type, status=status.value)
+    return p.pack() + r.pack() + (data or bytes(0))
+
+def handle_request(buf):
+    p, payload = PacketHead.unpack_from(buf)
+    checkmsg(p.protocol_version == PROTOCOL_VERSION, 'Invalid protocol version')
+    checkmsg(p.nonce[-1] & 0x1 == 0, 'Last bit of request nonce must be 0')
+    r, data = RequestHead.unpack_from(payload)
+    try:
+        t = MsgType(r.msg_type)
+    except ValueError:
+        raise BadMessageError('Unknown request type {}'.format(r.msg_type))
+    status, data = process_request[t](data)
+    return make_reply(p, r, status, data)
 
 if __name__ == '__main__':
     with open('./packet_example.bin', 'rb') as f:
-        readpacket(f.read())
+        reply = handle_request(f.read())
+        print(reply)
