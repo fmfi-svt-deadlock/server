a8984d1dd45d66d0dab5798f26d2a32e3cdda633
Kamila Součková <kamila@ksp.sk>
2015-02-16 21:18:32 +0100

    remove http_api & co.

--- runhttp.py
+++ runhttp.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """Gate HTTP API server runner."""
 
-from gateserver import db, http_api
-import config
+#from gateserver import db, http_api
+#import config
 
-if __name__ == '__main__':
-    db.connect(config.db_url)
-    http_api.serve(config)
+#if __name__ == '__main__':
+    #db.connect(config.db_url)
+    #http_api.serve(config)
+print('Not implemented yet.')
